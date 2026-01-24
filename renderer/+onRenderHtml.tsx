d073eeb16163ff2a56865c7004623a4115756b84
Your Name <you@example.com>
2026-01-25 02:24:24 +0530

    fix: Add updated favicon and manifest

--- renderer/+onRenderHtml.tsx
+++ renderer/+onRenderHtml.tsx
@@ -18,6 +18,12 @@ function onRenderHtml(pageContext: PageContextServer) {
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>${title ?? 'Vijaykoushik S | Zero-to-One Architect'}</title>
+          <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+          <link rel="shortcut icon" href="favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+          <meta name="apple-mobile-web-app-title" content="VJ" />
+          <link rel="manifest" href="site.webmanifest" />
           <link rel="preconnect" href="https://fonts.googleapis.com">
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
           <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;600&family=Inter:wght@300;400;600;800&display=swap" rel="stylesheet">
